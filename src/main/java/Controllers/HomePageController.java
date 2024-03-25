10b2f956177a677f072ed680ff16d73f5f1bb70f
Rezi <rezigagnidze2@gmail.com>
2024-03-25 04:58:40 +0400

    bug fixes, when uploading photos. added FXfunctions.java for global scope.

--- src/main/java/Controllers/HomePageController.java
+++ src/main/java/Controllers/HomePageController.java
@@ -1,13 +1,17 @@
 package Controllers;
 
+import GlobalScope.FXfunctions;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
 
 public class HomePageController {
 
@@ -18,7 +22,7 @@ public class HomePageController {
     private TextField messageTextField;
 
     @FXML
-    private Circle userProfileImage;
+    private Circle userProfileCircle;
 
     @FXML
     private GridPane homePageGrid;
@@ -31,16 +35,49 @@ public class HomePageController {
 
         try {
             Image image = new Image(HomePageController.class.getResource("/Images/modified.png").toExternalForm());
-            userProfileImage.setFill(new ImagePattern(image));
 
-            userProfileImage.getStyleClass().add("profile-pictures");
-
-            homePageGrid.add(uploadPhotoButton,0,1);
+            userProfileCircle.setFill(new ImagePattern(image));
+            userProfileCircle.getStyleClass().add("profile-pictures");
 
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
 
+    }
+
+    @FXML
+    private void uploadPhotoButtonClicked(){
+
+        Image image = FXfunctions.uploadImage((Stage) uploadPhotoButton.getScene().getWindow());
+
+        if(image == null) return;
+
+        userProfileCircle.setFill(new ImagePattern(image));
+
+    }
+
+    @FXML
+    private void messageTextFieldAction(){
+
+        if(messageTextField.getText().equals("")) return;
+
+        updateChatFlow(messageTextField.getText());
+        messageTextField.setText("");
+
+    }
+
+    private void updateChatFlow(String message){
+
+        ImagePattern imagePattern = (ImagePattern) userProfileCircle.getFill(); //getting image for circle
+
+        Circle profPic = new Circle(10, imagePattern); //getting circle to display picture in chat
+
+        Text text = new Text(message); //text itself
+
+        HBox hBox = new HBox(profPic, text); //add both in HBox to be on the same liens
+
+
+        chatTextFlow.getChildren().addAll(hBox, new Text("\n"));
     }
 
 }
---
+++ src/main/java/GlobalScope/FXfunctions.java
@@ -0,0 +1,36 @@
+package GlobalScope;
+
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class FXfunctions {
+
+    /** Uploads image from file chooser (.jpg, .jpeg, .png).
+     * @param stage current stage, where the filechooser is opening.
+     * @return returns the uploaded file if it is image.
+     */
+    public static Image uploadImage(Stage stage){
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Upload Image");
+
+        FileChooser.ExtensionFilter imageFilter =
+                new FileChooser.ExtensionFilter("Photos", "*.jpg", "*.jpeg", "*.png");
+
+        fileChooser.getExtensionFilters().add(imageFilter);
+
+        File file = fileChooser.showOpenDialog(stage);
+
+        if(file != null){
+            System.out.println("Image uploaded: " + file.getAbsolutePath());
+            Image image = new Image(file.toURI().toString());
+            return image;
+        }
+
+        return null;
+    }
+
+}
